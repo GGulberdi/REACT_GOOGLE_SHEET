d0ac926d4c2c14df3d25f3f3cbf115a195198a99
Gulsenem Gulberdi <ggulberdiyeva@gmail.com>
2021-09-15 10:03:08 -0500

    added form and fetced API from google sheet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+import { Button, Form, Table } from 'semantic-ui-react'
+import env from "react-dotenv";
+const App = () => {
+const [data,setData]=useState([]);
+const [firstname,setFirtsname]=useState("");
+const [lastname,setLastsname]=useState("");
+const [address,setAddress]=useState("");
+const [phone,setPhone]=useState("");
+const [refresh,setRefresh]=useState([]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+console.log(firstname,lastname,address,phone)
+
+
+
+
+const onSubmit= async(e)=>{
+  e.preventDefault();
+
+  try{
+   const response = await fetch(process.env.REACT_APP_API_KEY,{
+     method:'POST',
+     headers:{
+       'Content-Type':'application/json'
+     },
+     body:JSON.stringify([[firstname,lastname,address,phone]])
+   }
+   )
+   await response.json();
+   setFirtsname("")
+   setLastsname("")
+   setRefresh(data) 
+  }catch(err){
+    console.log(err)
+  }
+  
 }
+
+// useEffect(()=>{
+//   axios.get(process.env.REACT_APP_API_KEY)
+//   .then((data)=>{
+//     console.log(data.data.data)
+//     setData(data.data.data)
+//   })
+//   .catch((err)=>console.log(err))
+// },[refresh])
+
+  return(
+    <div>
+<h1 style={{padding:'5% 20% 1% 20%', textAlign:'center'}}>ORDER FORM</h1>
+          <Form style={{padding:'1% 20%'}}>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='Name' 
+      onChange={(e)=> setFirtsname(e.target.value)}/>
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' 
+      onChange={(e)=> setLastsname(e.target.value)}/>
+    </Form.Field>
+    <Form.Field>
+      <label>Address</label>
+      <input placeholder='Address'
+      onChange={(e)=> setAddress(e.target.value)}/>
+          </Form.Field>
+    <Form.Field>
+      <label>Phone number</label>
+      <input placeholder='Phone number' 
+      onChange={(e)=> setPhone(e.target.value)}/>
+          </Form.Field>
+    <Button type='submit' onClick={onSubmit}>Submit</Button>
+  </Form>
+
+
+  <Table style={{padding:'5% 20%'}}>
+    <Table.Header >
+      <Table.Row>
+      <Table.HeaderCell  style={{backgroundColor: 'yellow'}}>Number</Table.HeaderCell>
+       <Table.HeaderCell  style={{backgroundColor: 'yellow'}}>First Name</Table.HeaderCell>
+        <Table.HeaderCell  style={{backgroundColor: 'yellow'}}>Last Name</Table.HeaderCell>
+        <Table.HeaderCell  style={{backgroundColor: 'yellow'}}>Address</Table.HeaderCell>
+        <Table.HeaderCell style={{backgroundColor: 'yellow'}}>Phone </Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+        {data.map((item)=>
+        (
+      <Table.Row key={item.row_id}>
+        <Table.Cell>{item.row_id+1}</Table.Cell>
+        <Table.Cell>{item.firstname}</Table.Cell>
+        <Table.Cell>{item.lastname}</Table.Cell>
+        <Table.Cell>{item.address}</Table.Cell>
+        <Table.Cell>{item.phone}</Table.Cell>
+        </Table.Row>
+        ))}
+      </Table.Body>
+  </Table>
+</div>
+  )
+}
+
 
 export default App;
